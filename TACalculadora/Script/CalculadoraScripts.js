1a5970fa05bd829839006ae15d9e37b87fb2ae1d
Jonas Correia <jonas149.correia@gmail.com>
2025-08-27 19:11:48 -0300

    Inicia projeto BDD da calculadora com TestComplete
    Adiciona os arquivos centrais do projeto TACalculadora no TestComplete. Inclui configurações, scripts, event handlers, keyword tests, name mapping e logs, estruturando a base para os testes automatizados em BDD da calculadora.

---
+++ TACalculadora/Script/CalculadoraScripts.js
@@ -0,0 +1,15 @@
+﻿function somar(){
+  Log.Message("Realizar soma aqui.")
+}
+
+function subtrair(){
+  Log.Message("Realizar subtração aqui.")
+}
+
+function multiplicar(){
+  Log.Message("Realizar multiplicação aqui.")
+}
+
+function dividir(){
+  Log.Message("Realizar divisão aqui.")
+}
